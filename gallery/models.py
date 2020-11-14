0143cfb6ab618af5f6d0baee7f746ae7ad13151c
kev127 <kelvinkeya125@gmail.com>
2020-11-14 13:46:50 +0300

    add image models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.model):
+    image=models.ImageField(upload_to = 'gallery/')
+    image_name=models.CharField(max_length =60)
+    image_description=models.TextField()
+    image_location=models.ForeignKey('Location',on_delete=models.CASCADE)
+    image_category=models.ForeignKey('Category',on_delete=models.CASCADE)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_image(cls):
+        all_images = cls.objects.all()
+        return all_images
+
+    @classmethod
+    def search_image(cls,category):
+        photos = cls.objects.filter(image_category__name__icontains=category) 
+        return photos      
+
+    @classmethod
+    def filter_by_location(cls,id):
+        image_by_location = cls.objects.filter(image_location_id = id)  
+        return image_by_location
+
+
