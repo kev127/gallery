edb89d85f485f5b049bd79baa8a7f0bd0e383dbe
kev127 <kelvinkeya125@gmail.com>
2020-11-15 09:30:23 +0300

    add search image view function

--- gallery/views.py
+++ gallery/views.py
@@ -15,3 +15,15 @@ def gallery_by_category(request, category_id):
     images = Image.filter_by_category(category_id) 
     return render(request,'all-gallery/category.html',{"images":images}) 
 
+def search_image(request):
+
+    if 'gallery' in request.GET and request.GET["gallery"]:
+        category= request.GET.get("gallery")
+        searched_images =Image.search_image(category)
+        message = f"{search_term}"
+
+        return render(request, 'all-gallery/search.html',{"message":message, "gallery":searched_images})
+
+    else:
+        message = "You haven't searched for any image"
+        return render(request, 'all-gallery/search.html',{"message":message})
