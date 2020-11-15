b6d24a4253786e5eaf978393d35240a1128e93aa
kev127 <kelvinkeya125@gmail.com>
2020-11-15 09:04:50 +0300

    add view function for gallery

--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,17 @@
-from django.http import HttpResponse 
-import datetime as dt
 from django.http  import HttpResponse,Http404
 from django.shortcuts import render,redirect
+from .models import Image,Category
 
 # Create your views here.
 def gallery_today(request):
     return render(request, 'all-gallery/today-gallery.html')
+
+def gallery(request):
+    images = Image.get_images()
+    categories = Category.objects.all()
+    return render(request, 'all-gallery/today-gallery.html',{"images":images,"category":category})
+
+def gallery_by_category(request, category_id):
+    images = Image.filter_by_category(category_id) 
+    return render(request,'all-gallery/category.html',{"images":images}) 
 
