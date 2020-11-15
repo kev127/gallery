fa65f18b9d5e14505c228a9427b701a93e004235
kev127 <kelvinkeya125@gmail.com>
2020-11-15 09:42:02 +0300

    add static route to reference location of uploaded files

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,3 +5,5 @@ urlpatterns=[
     path('',views.gallery_today,name = 'galleryToday'),
     path('search/', views.search_image, name='search_image')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
