18ecc3bec40b3b615f8a6a292b5712e70daa71b4
kev127 <kelvinkeya125@gmail.com>
2020-11-15 20:18:25 +0300

    update code

--- gallery/forms.py
+++ gallery/forms.py
@@ -1,6 +1,15 @@
 from django.forms import ModelForm      
 from .models import Image
+from cloudinary.forms import CloudinaryFileField    
 
 class ImageForm(ModelForm):
   class Meta:
       model = Image
+      fields = '__all__'
+  image = CloudinaryFileField(
+        # attrs = { 'style': "margin-top: 30px" }, 
+        options = { 
+            'tags': "directly_uploaded",
+            'crop': 'limit', 'width': 1000, 'height': 1000,
+            'eager': [{ 'crop': 'fill', 'width': 150, 'height': 100 }]
+        })
