389ac641ce7d0eb62d9eca08cfb312f3ebae9040
kev127 <kelvinkeya125@gmail.com>
2020-11-17 09:28:28 +0300

    add cloudinary

--- gallery/models.py
+++ gallery/models.py
@@ -5,7 +5,7 @@ import cloudinary
 
 # Create your models here.
 class Image(models.Model):
-    image=models.ImageField(upload_to = 'gallery/')
+    image = CloudinaryField('image')
     image_name=models.CharField(max_length =60)
     image_description=models.TextField()
     image_location=models.ForeignKey('Location',on_delete=models.CASCADE)
